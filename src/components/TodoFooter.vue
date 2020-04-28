166110c0728b931fb52a1c28bd78c23cdc64078e
yongho1 <skyks4848@naver.com>
2020-04-28 23:48:23 +0900

    20200428

--- src/components/TodoFooter.vue
+++ src/components/TodoFooter.vue
@@ -1,10 +1,31 @@
 <template>
-  <div>Footer</div>
+  <div class="clearAllContainer">
+    <span class="clearAllBtn" v-on:click="clearTodo">Clear All</span>
+  </div>
 </template>
 
+
 <script>
-export default {};
+export default {
+  methods: {
+    clearTodo: function() {
+      localStorage.clear();
+    }
+  }
+};
 </script>
 
-<style>
+<style scoped>
+.clearAllContainer {
+  width: 8.5rem;
+  height: 50px;
+  margin: 0 auto;
+  line-height: 50px;
+  border-radius: 5px;
+  background-color: white;
+}
+.clearAllBtn {
+  display: block;
+  color: #e20303;
+}
 </style>
